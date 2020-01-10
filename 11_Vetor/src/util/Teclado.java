48113a2fb76d8d51a2a8b0848f1313df66623f6c
Aluno <Aluno@ALU22206.educa.sf>
2020-01-10 13:34:34 -0300

    Vetor_11

---
+++ 11_Vetor/src/util/Teclado.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Teclado {
+
+	/**
+	 * Objeto que representa o teclado
+	 */
+	private static BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+	public static String lerTexto (String texto) {
+		try {
+			// Mostrar o texto
+			System.out.println(texto);
+			
+			// Lê a linha
+			return teclado.readLine();
+				} catch (IOException e) {
+					return null;
+				}
+	}
+
+	public static int lerInt(String texto) {
+		// Chama o método lerString e converte o resultado 
+		// para inteiro
+		return Integer.parseInt (lerTexto(texto));
+		
+	}
+
+
+public static double lerDouble (String texto) { 
+	// Chama o método lerString e converter o 
+	// resultado para double
+	return Double.parseDouble(lerTexto(texto));
+}
+}
+
